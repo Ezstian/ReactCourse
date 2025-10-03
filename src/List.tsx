774bd0cfadeb680db38280983476e2ed98fc1810
Ezstian <krystiankc243@gmail.com>
2025-10-03 13:49:27 +0200

    more render lists

--- src/List.tsx
+++ src/List.tsx
@@ -1,12 +1,17 @@
 function List(){
     const fruits = [{id:1, name:"apple",calories: 95},
-                            {id:2 ,name:"orange",calories: 56},
-                            {id:3 ,name:"banana", calories: 34}];
+                            {id:2 ,name:"orange",calories: 126},
+                             {id:3 ,name:"pineapple", calories: 134},
+                            {id:4 ,name:"watermelon", calories: 54},
+                            {id:5 ,name:"banana", calories: 34}];
 
-    fruits.sort();
-
-    const listItems = fruits.map(fruits=>
-        <li key={fruits.id}>{fruits.name}</li>);
+    //fruits.sort((a,b)=>a.name.localeCompare(b.name));//ALPHABETICAL
+    //fruits.sort((a,b)=>b.name.localeCompare(a.name); //REVERSE ALPHABETICAL
+    //fruits.sort((a,b)=>a.calories - b.calories); //Numeric order
+    const lowCalFruit = fruits.filter(fruit => fruit.calories < 100);
+    const listItems = lowCalFruit.map(lowCalFruit=>
+        <li key={lowCalFruit.id}>
+            {lowCalFruit.name}: &nbsp;{lowCalFruit.calories} calories</li>);
     return (<ol>{listItems}</ol>);
 };
 
